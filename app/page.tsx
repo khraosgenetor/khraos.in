f2c698b235be1e8f1a26fef9055630daab5bc128
khraos <khraos@khraos.in>
2025-11-25 21:57:16 +0530

    Second Commit: "Added my profile picture, css for a vertical bar, fixed the buttons."

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,31 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/svgs/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            khraos.in is undergoing renovations. Please stand by.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            The website is being rewritten from scratch due to issues while programming.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+              <h1 className="flex items-center gap-4 text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                  <span>khraos.in is undergoing renovations.<br />Please stand by.</span>
+                  <div className="vertical-line right"></div>
+                  <span>
+                      <Image
+                          className="rounded-full border-2 border-gray-400"
+                          src="/pictures/pfp.jpg"
+                          alt="profile picture"
+                          width={200}
+                          height={100}
+                      />
+                  </span>
+              </h1>
+              <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                  The website is being rewritten from scratch due to issues while programming.
+              </p>
+          </div>
+          <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="/ghrepo"
@@ -29,20 +39,27 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              className=""
+              src="/svgs/github.svg"
+              alt="GH logomark"
+              width={24}
+              height={24}
             />
-            GitHub Repo
+            GitHub
           </Link>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://www.instagram.com/khraosgenetor/"
             target="_blank"
             rel="noopener noreferrer"
           >
+              <Image
+                  className=""
+                  src="/svgs/insta.svg"
+                  alt="Insta logomark"
+                  width={22}
+                  height={22}
+              />
             Instagram
           </a>
         </div>
