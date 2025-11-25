ac3bb826259e5589377b6668f3e3c3336d7fb953
khraos <khraos@khraos.in>
2025-11-25 16:52:22 +0530

    Initial Commit: "Cleaning up home page"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async redirects() {
+        return [
+            {
+                source: "/ghrepo",
+                destination: "https://github.com/khraosgenetor/khraos.in",
+                permanent: true
+            }
+        ];
+    }
 };
 
 export default nextConfig;
